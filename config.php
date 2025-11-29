c66517bc13c97a2e762cfb05a173fe48aab41fda
milk-palace <forstuffthatidodude@gmail.com>
2025-11-29 21:20:45 +0700

    config.php added
    config.php initial release

---
+++ config.php
@@ -0,0 +1,15 @@
+﻿<?php
+session_start();
+
+$host = "[sqlserver]";
+$dbname = "[database name]";
+$username = "[cPanel/DB Username]";
+$password = "[cPanel/DB Password]";   // <- your DB / cPanel password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
